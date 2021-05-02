f04624d132229ef187b913138c8b086993826705
Abhilash Kulkarni <62510765+abhilashkulkarniofficial@users.noreply.github.com>
2021-05-02 14:56:31 +0530

    Added email template (#8)
    * Changed email body
    
    Added html email template to the email body to display the slots information in a cleaner template.
    
    * Removed bug in creating template
    
    * Updated Readme to add new email template

--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -53,7 +53,7 @@ function getSlotsForDate(DATE) {
             let validSlots = sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0)
             console.log({date:DATE, validSlots: validSlots.length})
             if(validSlots.length > 0) {
-                notifyMe(validSlots);
+                notifyMe(validSlots, DATE);
             }
         })
         .catch(function (error) {
@@ -63,9 +63,9 @@ function getSlotsForDate(DATE) {
 
 async function
 
-notifyMe(validSlots){
+notifyMe(validSlots, date){
     let slotDetails = JSON.stringify(validSlots, null, '\t');
-    notifier.sendEmail(EMAIL, 'VACCINE AVAILABLE', slotDetails, (err, result) => {
+    notifier.sendEmail(EMAIL, 'VACCINE AVAILABLE', slotDetails, date, (err, result) => {
         if(err) {
             console.error({err});
         }
